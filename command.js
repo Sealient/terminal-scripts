229fa684d99268a7013e20308a6db41a471eaa70
SΞΛL. <snowflakezr@gmail.com>
2024-12-22 20:02:41 -0800

    Update command.js

--- command.js
+++ command.js
@@ -1,22 +1,4 @@
-window.availableCommands = window.availableCommands || {};
-
-Object.assign(window.availableCommands, {
-    help: () => {
-        const output = `
-            Available commands:
-            - help: Displays this message
-            - whoami: Shows who you are
-        `;
-        const outputElement = document.querySelector(".output");
-        const p = document.createElement("p");
-        p.textContent = output;
-        outputElement.appendChild(p);
-    },
-    whoami: () => {
-        const output = "You are a user of this terminal.";
-        const outputElement = document.querySelector(".output");
-        const p = document.createElement("p");
-        p.textContent = output;
-        outputElement.appendChild(p);
-    },
-});
+export default function () {
+    console.log("Custom command.js package loaded.");
+    alert("Custom command from your GitHub has been executed!");
+}
